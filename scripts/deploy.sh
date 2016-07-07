77d4135d45bdd9529f26473042b81dd848c72915
Cody Reichert <cody@reichertbrothers.com>
2016-07-07 10:37:04 -0500

    Update scripts/deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,7 @@ then
   exit 0
 fi
 
+# Create a deploy and get the ID so we can update it later
 DEPLOY_ID=$(curl -XPOST "https://$GH_TOKEN@api.github.com/repos/$TRAVIS_REPO_SLUG/deployments" -H "Content-Type:application/json" --data '{"ref":"master"}' | python2 -c "import json,sys;obj=json.load(sys.stdin);print obj['id'];")
 
 rev=$(git rev-parse --short HEAD)
