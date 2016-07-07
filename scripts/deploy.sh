4f8c063573bac879e3a6032a52b8cb0fb4e3e6a0
Cody Reichert <cody@reichertbrothers.com>
2016-07-07 11:20:49 -0500

    Fix python executable in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ DEPLOY_URL="https://$GH_TOKEN@api.github.com/repos/$TRAVIS_REPO_SLUG/deployments
 echo $DEPLOY_URL
 
 # Create a deploy and get the ID so we can update it later
-DEPLOY_ID=$(curl -XPOST $DEPLOY_URL -H "Content-Type:application/json" --data '{"ref":"master"}' | python2 -c "import json,sys;obj=json.load(sys.stdin);print obj['id'];")
+DEPLOY_ID=$(curl -XPOST --verbose $DEPLOY_URL -H "Content-Type:application/json" --data '{"ref":"master"}' | python -c "import json,sys;obj=json.load(sys.stdin);print obj['id'];")
 
 echo $DEPLOY_ID
 
@@ -46,5 +46,5 @@ git push -q upstream HEAD:gh-pages
 
 # Update the deploy as success
 echo "Updating DEPLOYMENT: $DEPLOY_ID"
-DEPLOYED=$(curl -XPOST "https://$GH_TOKEN@api.github.com/repos/$TRAVIS_REPO_SLUG/deployments/$DEPLOY_ID/statuses" --data '{"state":"success"}' | python2 -c "import json,sys;obj=json.load(sys.stdin);print obj['state'];")
+DEPLOYED=$(curl -XPOST "https://$GH_TOKEN@api.github.com/repos/$TRAVIS_REPO_SLUG/deployments/$DEPLOY_ID/statuses" --data '{"state":"success"}' | python -c "import json,sys;obj=json.load(sys.stdin);print obj['state'];")
 echo $DEPLOYED
