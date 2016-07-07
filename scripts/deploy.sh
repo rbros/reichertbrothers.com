5ce0a91ab71de980a7d5960fe98a502c1e2bd5a1
Cody Reichert <cody@reichertbrothers.com>
2016-07-07 09:18:05 -0500

    Update deploy.sh to create github deployment status

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,8 @@ then
   exit 0
 fi
 
+DEPLOY_ID=$(curl -XPOST "https://$GH_TOKEN@api.github.com/repos/$TRAVIS_REPO_SLUG/deployments" -H "Content-Type:application/json" --data '{"ref":"master"}' | python2 -c "import json,sys;obj=json.load(sys.stdin);print obj['id'];")
+
 rev=$(git rev-parse --short HEAD)
 
 # Make a directory where we can build the files We will CD into this
@@ -35,3 +37,7 @@ cp -R $TRAVIS_BUILD_DIR/_site/* .
 git add -A .
 git commit -m "rebuild pages at ${rev}"
 git push -q upstream HEAD:gh-pages
+
+# Update the deploy as success
+DEPLOYED=$(curl -XPOST "https://$GH_TOKEN@api.github.com/repos/$TRAVIS_REPO_SLUG/deployments/$DEPLOY_ID/statuses" --data '{"state":"success"}' | python2 -c "import json,sys;obj=json.load(sys.stdin);print obj['state'];")
+echo $DEPLOYED
